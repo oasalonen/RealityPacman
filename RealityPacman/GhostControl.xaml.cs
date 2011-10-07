df8a4709f88531f180104026b98801f2784f6d61
Olli Mahlamaki <olli.mahlamaki@gmail.com>
2011-10-07 18:18:19 +0200

    Ghost is now animated.

--- RealityPacman/GhostControl.xaml.cs
+++ RealityPacman/GhostControl.xaml.cs
@@ -9,14 +9,35 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace RealityPacman
 {
     public partial class GhostControl : UserControl
     {
+        private DispatcherTimer animTimer;
+        private int frame = 0;
+        private int delta = 1;
+
         public GhostControl()
         {
             InitializeComponent();
+
+            animTimer = new DispatcherTimer();
+            animTimer.Interval = TimeSpan.FromMilliseconds(50);
+            animTimer.Tick += new EventHandler(animTimer_Tick);
+
+            animTimer.Start();
+        }
+
+        void animTimer_Tick(object sender, EventArgs e)
+        {
+            frame = frame + delta;
+            if (frame == 10)
+                delta = -1;
+            else if (frame == 0)
+                delta = 1;
+            Canvas.SetLeft(sprite, -64 * frame);
         }
     }
 }
